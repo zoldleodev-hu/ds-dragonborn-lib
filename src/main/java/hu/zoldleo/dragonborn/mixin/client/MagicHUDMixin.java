e80ce5d3a8af1ca0526a189a1b40ba18ac978d5d
ZoldLeo <zoldleogameplay@gmail.com>
2026-04-10 10:15:34 +0200

    1.1.10: Fixed jump bar

---
+++ src/main/java/hu/zoldleo/dragonborn/mixin/client/MagicHUDMixin.java
@@ -0,0 +1,19 @@
+package hu.zoldleo.dragonborn.mixin.client;
+
+import by.dragonsurvivalteam.dragonsurvival.client.gui.hud.MagicHUD;
+import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import hu.zoldleo.dragonborn.util.DragonbornUtils;
+import net.minecraft.world.entity.PlayerRideableJumping;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MagicHUD.class)
+public class MagicHUDMixin {
+    @ModifyExpressionValue(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lby/dragonsurvivalteam/dragonsurvival/common/capability/DragonStateHandler;isDragon()Z"))
+    private static boolean dontRenderOnHorse(boolean original, @Local(name = "handler") DragonStateHandler handler, @Local(name = "localPlayer") Player localPlayer) {
+        return original && !(DragonbornUtils.isDragonborn(handler) && localPlayer.getControlledVehicle() instanceof PlayerRideableJumping playerrideablejumping && playerrideablejumping.canJump());
+    }
+}
