899e905ba1036d7bcad3a78bcc051e3dd42fc9ee
ZoldLeo <zoldleogameplay@gmail.com>
2026-08-04 08:08:33 +0200

    Fix altar button, animations

--- src/main/java/hu/zoldleo/dragonborn/mixin/client/AltarTypeButtonMixin.java
+++ src/main/java/hu/zoldleo/dragonborn/mixin/client/AltarTypeButtonMixin.java
@@ -1,14 +1,18 @@
 package hu.zoldleo.dragonborn.mixin.client;
 
+import by.dragonsurvivalteam.dragonsurvival.DragonSurvivalMod;
 import by.dragonsurvivalteam.dragonsurvival.client.gui.widgets.buttons.AltarTypeButton;
 import by.dragonsurvivalteam.dragonsurvival.common.dragon_types.AbstractDragonType;
 import by.dragonsurvivalteam.dragonsurvival.common.dragon_types.DragonTypes;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Arrays;
 
@@ -24,17 +28,11 @@ public class AltarTypeButtonMixin {
         return new ResourceLocation("dragonsurvival", "textures/gui/custom/altar/" + type.getTypeNameLowerCase() + "/altar_icon.png");
     }
 
-    @ModifyArg(method = "renderWidget", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V"))
-    private ResourceLocation customTexture2(ResourceLocation loc) {
-        if (Arrays.asList(null, DragonTypes.CAVE, DragonTypes.FOREST, DragonTypes.SEA).contains(type))
-            return loc;
-        return new ResourceLocation("dragonsurvival", "textures/gui/custom/altar/" + type.getTypeNameLowerCase() + "/altar_icon.png");
-    }
-
-    @ModifyVariable(method = "renderWidget", at = @At(value = "STORE", ordinal = 0), name = "uOffset")
-    private int noOffset(int uOffset) {
-        if (type != null)
-            return 0;
-        return uOffset;
+    @WrapOperation(method = "renderWidget", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V"))
+    private void renderCustomBanner(GuiGraphics instance, ResourceLocation texture, int x, int y, float u, float v, int du, int dv, int texWidth, int texHeight, Operation<Void> original, @Local(name = "uOffset") int uOffset) {
+        if (uOffset == 3 && type != null)
+            instance.blit(DragonSurvivalMod.res("textures/gui/custom/altar/" + type.getTypeNameLowerCase() + "/altar_icon.png"), x, y, 0, ((AltarTypeButton)(Object)this).isHovered() ? 0 : 147, 49, 147, 49, 294);
+        else
+            original.call(instance, texture, x, y, u, v, du, dv, texWidth, texHeight);
     }
 }
